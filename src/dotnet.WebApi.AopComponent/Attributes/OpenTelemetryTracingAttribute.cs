cc1f0035e552a692bede0b7488bbc30985ab85ec
YuChia <h42279@gmail.com>
2022-12-28 21:47:41 +0800

    Add Aop Component

---
+++ src/dotnet.WebApi.AopComponent/Attributes/OpenTelemetryTracingAttribute.cs
@@ -0,0 +1,9 @@
+﻿using AspectInjector.Broker;
+using dotnet.WebApi.AopComponent.Aspect;
+
+namespace dotnet.WebApi.AopComponent.Attributes;
+
+[Injection(typeof(OpenTelemetryTracingMethodAspect))]
+public class OpenTelemetryTracingAttribute : Attribute
+{
+}
