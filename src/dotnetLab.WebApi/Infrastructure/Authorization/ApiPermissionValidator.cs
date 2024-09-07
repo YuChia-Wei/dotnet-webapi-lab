ebdfe1c5a178ad0a72226df2eb9504d39d547ca6
YuChia <h42279@gmail.com>
2024-09-07 22:08:37 +0800

    調整物件說明

--- src/dotnetLab.WebApi/Infrastructure/Authorization/ApiPermissionValidator.cs
+++ src/dotnetLab.WebApi/Infrastructure/Authorization/ApiPermissionValidator.cs
@@ -1,8 +1,11 @@
 namespace dotnetLab.WebApi.Infrastructure.Authorization;
 
 /// <summary>
-/// permission validator
+/// permission validator Fake implement
 /// </summary>
+/// <remarks>
+/// 你可以自己實做一個獨立的 api 權限驗證器
+/// </remarks>
 public class ApiPermissionValidator : IApiPermissionValidator
 {
     /// <summary>
