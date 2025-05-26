ce1fbfd6e527a2c53d6f6cc42f6e6e638363419d
YuChia <h42279@gmail.com>
2025-05-26 16:03:01 +0800

    調整注入做法

--- src/dotnetLab.UseCase/ServiceCollectionExtension.cs
+++ src/dotnetLab.UseCase/ServiceCollectionExtension.cs
@@ -12,7 +12,8 @@ public static class ServiceCollectionExtension
         serviceCollection.AddWolverine(options =>
         {
             // 指定組件位置
-            options.Discovery.IncludeAssembly(typeof(SimpleDocQuery).Assembly);
+            // 因為目前類別就位於 use case，因此指定自己，避免因業務邏輯的變更而影響元件掃描
+            options.Discovery.IncludeAssembly(typeof(dotnetLab.UseCase.ServiceCollectionExtension).Assembly);
         });
         // 另外的註冊方式是要使用 builder host 去註冊，讓系統去掃描所有有使用到 IMessageBus 介面，或有符合 Handler 實作命名的類別並自動註冊
         // builder.Host.UseWolverine();
